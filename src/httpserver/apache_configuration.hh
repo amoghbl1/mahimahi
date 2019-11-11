6c86e30201ceefc7cbcbf0e13cb3d5ba5235718a
Amogh Pradeep <amoghbl1@gmail.com>
2019-11-11 17:43:57 -0500

    apache configuration was incomplete.

--- src/httpserver/apache_configuration.hh
+++ src/httpserver/apache_configuration.hh
@@ -7,7 +7,11 @@
 #ifndef APACHE_CONFIGURATION_HH
 #define APACHE_CONFIGURATION_HH
 
-const std::string apache_main_config = "LoadModule mpm_prefork_module " + std::string( MOD_MPM_PREFORK ) + "\nLoadModule authz_core_module " + std::string( MOD_AUTHZ_CORE ) + "\nMutex pthread\nLoadFile " + std::string( MOD_DEEPCGI ) + "\nLoadModule deepcgi_module " + std::string( MOD_DEEPCGI ) + "\nSetHandler deepcgi-handler\n";
+const std::string apache_main_config = "LoadModule mpm_prefork_module " + std::string( MOD_MPM_PREFORK ) + "\nLoadModule authz_core_module " + std::string( MOD_AUTHZ_CORE ) + "\nMutex pthread\nLoadFile " + std::string( MOD_DEEPCGI ) + "\nLoadModule deepcgi_module " + std::string( MOD_DEEPCGI ) +
+    "\nLoadModule log_config_module modules/mod_log_config.so" +
+    "\nLoadModule unixd_module modules/mod_unixd.so" +
+    "\nLoadModule access_compat_module modules/mod_access_compat.so" +
+    "\nSetHandler deepcgi-handler\n";
 
 const std::string apache_ssl_config = "LoadModule ssl_module " + std::string( MOD_SSL ) + "\nSSLEngine on\nSSLCertificateFile      " + std::string( MOD_SSL_CERTIFICATE_FILE ) + "\nSSLCertificateKeyFile " + std::string( MOD_SSL_KEY ) +"\n";
 
